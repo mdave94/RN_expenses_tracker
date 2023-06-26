ea07b0edfb1dce58d61f41b10fb6c7f54cc1b86a
Molnar Dávid <molnardavid2002@gmail.com>
2023-06-26 13:04:08 +0200

    More components are added

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+function ExpensesSummary(params) {
+  return (
+    <View>
+      <Text>Last 7 days</Text>
+      <Text>$17</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;

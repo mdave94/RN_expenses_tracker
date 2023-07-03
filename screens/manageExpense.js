25d59b95db40374eee00fd9c4ac1d9e0815cfb32
Molnar Dávid <molnardavid2002@gmail.com>
2023-07-03 15:06:55 +0200

    edit and add expense option is added for navigation

--- screens/manageExpense.js
+++ screens/manageExpense.js
@@ -1,6 +1,18 @@
 import { View, Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
+function ManageExpense({ route, navigation }) {
+  // ? is optional operator. If params is undefined expenseId wont used and the expression'll return undefined
+  const editedExpenseId = route.params?.expenseId;
+  //js version to convert data to boolian
+  // true for editing false to not
+  const isEditing = !!editedExpenseId;
 
-function ManageExpense() {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View style={styles.container}>
       <Text>ManageExpense Page</Text>

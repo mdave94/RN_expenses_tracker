4a8a6aab98205ce1feb13e53e3895a173066026b
Molnar Dávid <molnardavid2002@gmail.com>
2023-07-04 14:26:49 +0200

    Button halders are added

--- App.js
+++ App.js
@@ -89,7 +89,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#",
     alignItems: "center",
     justifyContent: "center",
   },
--- screens/manageExpense.js
+++ screens/manageExpense.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet } from "react-native";
 import { useLayoutEffect } from "react";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/style";
+import Button from "../components/UI/Button";
 function ManageExpense({ route, navigation }) {
   // ? is optional operator. If params is undefined expenseId wont used and the expression'll return undefined
   const editedExpenseId = route.params?.expenseId;
@@ -15,10 +16,28 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  function deleteExpressHandler() {}
+  function deleteExpressHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -39,13 +58,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    backgroundColor: GlobalStyles.colors.primary500,
+    backgroundColor: GlobalStyles.colors.primary700,
   },
   deleteContainer: {
     marginTop: 16,
     marginTop: 8,
     borderTopWidth: 2,
     borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
   },
 });

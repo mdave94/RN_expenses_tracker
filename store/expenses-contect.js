ab33b1a9a302eb6019ed49c48a784fbe78bd1cbd
Molnar Dávid <molnardavid2002@gmail.com>
2023-07-04 21:51:47 +0200

    ContextApi is added for this project

---
+++ store/expenses-contect.js
@@ -0,0 +1,71 @@
+import { createContext, useReducer } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "something",
+    amount: 11.82,
+    date: new Date("2021-11-02"),
+  },
+  {
+    id: "e2",
+    description: "somehting else",
+    amount: 311.82,
+    date: new Date("2011-11-22"),
+  },
+  {
+    id: "e3",
+    description: "new thing",
+    amount: 123.82,
+    date: new Date("2022-01-2"),
+  },
+];
+
+//uppercase bc I'll use it as a component
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
+
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      const id = new Date().toString + Math.random.toString();
+      return [{ ...action.payload, id: id }, ...state];
+    case "UPDATE":
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex];
+      const updatedItem = { ...updatableExpense, ...action.payload.data };
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
+    case "DELETE":
+      return state.filter((expense) => expense.id !== action.payload);
+    default:
+      return state;
+  }
+}
+
+function ExpenseContextProvider({ children }) {
+  const [expenseState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+
+  function addExpense(expenseData) {
+    dispatch({ type: "ADD", payload: expenseData });
+  }
+
+  function deleteExpense(id) {
+    dispatch({ type: "DELETE", payload: id });
+  }
+
+  function updateExpense(id, expenseData) {
+    dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
+  }
+
+  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+}
+
+export default ExpenseContextProvider;
